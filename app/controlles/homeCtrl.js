4587147288106652bdbd0910e9527c9a8fbfd9db
Andru <Andru>
2016-06-07 00:55:48 +0200

    homepage

--- app/controlles/homeCtrl.js
+++ app/controlles/homeCtrl.js
@@ -1,3 +1,33 @@
-app.controller( 'HomeCtrl', function($scope) {
-     $scope.title = 'Strona główna';
+app.controller('HomeCtrl', function($scope) {
+    $scope.title = 'Strona główna';
+    $scope.projects = [
+        {
+            thumb: 'images/mockup/abstract-q-c-640-480-1.jpg',
+            image: 'images/mockup/abstract-q-c-640-480-1.jpg',
+            title: 'tytuł 1',
+            description: {
+                jobs: 'zakres prac 1',
+                tech: 'Technologie 1'
+            },
+            url: {
+                href: 'www link 1',
+                text: 'www tekst 1',
+                title: 'www title 1'
+            }
+        },
+        {
+            thumb: 'images/mockup/abstract-q-c-640-480-2.jpg',
+            image: 'images/mockup/abstract-q-c-640-480-2.jpg',
+            title: 'tytuł 2',
+            description: {
+                jobs: 'zakres prac 2',
+                tech: 'Technologie 2'
+            },
+            url: {
+                href: 'www link 2',
+                text: 'www tekst 2',
+                title: 'www title 2'
+            }
+        }
+    ]
 });
