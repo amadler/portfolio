e76218c328a0e40343101fd54a83db25363a6b5d
Andru <Andru>
2016-06-06 20:17:19 +0200

    konfiguracja gulpa

--- gulpfile.js
+++ gulpfile.js
@@ -1,49 +1,42 @@
 // run gulpfile-install to install modules
 var plugins = require('gulp-load-plugins')(),
     gulp = require('gulp'),
-    concat = require('gulp-concat'),
     sass = require('gulp-sass'),
-    path = require('path'),
-    rename = require('gulp-rename'),
-    minify = require('gulp-uglify'),
-    minifyCss = require('gulp-minify-css'),
-    gutil = require('gulp-util'),
-    gulpJade    = require('gulp-jade'),
-    ext_replace = require('gulp-ext-replace'),
-    path_sass = './sass/**/*.scss',
-    path_sassfile = './sass/style.scss',
-    // path_jade = '../assets/css/',
-    // path_css = '../assets/css/',
+    // path = require('path'),
+    // rename = require('gulp-rename'),
+    // minify = require('gulp-uglify'),
+    // minifyCss = require('gulp-minify-css'),
+    // gutil = require('gulp-util'),
+    gulpJade = require('gulp-jade'),
+    // ext_replace = require('gulp-ext-replace'),
+    path_sass = './app/sass/**/*.scss',
+    path_sassfile = './app/sass/style.scss',
     path_css = './app/css/',
+    path_jade = './app/jade/**/*.jade',
+    path_templates = './app/templatess/',
     path_js = ''
-    gulp.task('jade', function(){
-    	return gulp.src('./jade/**/*.jade')
-    		.pipe(gulpJade({
-    			pretty:true
-    		}))
-        .pipe(ext_replace('.html.twig'))
-    		.pipe(gulp.dest('../views'))
-    })
-    gulp.task('change', function() {
-      gulp.src('./twig/**/*.html')
-          .pipe(rename('twig'))
-          .pipe(gulp.dest('twig'))
-    });
+gulp.task('jade', function() {
+    return gulp.src(path_jade)
+        .pipe(gulpJade({
+            pretty: true
+        }))
+        .pipe(gulp.dest(path_templates))
+})
 
-    gulp.task('autoprefixer', function () {
-      return gulp.src(path_sass)
-          .pipe(plugins.autoprefixer({
-              browsers: ['last 2 versions'],
-              cascade: false
-          }))
-          .pipe(gulp.dest(path_css));
-    });
-    gulp.task('sass', function () {
-      return gulp.src(path_sassfile)
+gulp.task('autoprefixer', function() {
+    return gulp.src(path_sass)
+        .pipe(plugins.autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest(path_css));
+});
+gulp.task('sass', function() {
+    return gulp.src(path_sassfile)
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest(path_css));
-    });
-    gulp.task('watch', function () {
-      gulp.watch( path_sass , ['sass']);
-      gulp.watch( './jade/**/*.jade' , ['jade']);
-    });
+});
+gulp.task('watch', function() {
+    gulp.watch(path_jade, ['jade']);
+    gulp.watch(path_sass, ['sass']);
+});
